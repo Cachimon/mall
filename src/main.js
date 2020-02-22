8f1794537119f9bd9f240bc978c5a1b804cbf6b3
cachimon <cachimon@foxmail.com>
2020-02-22 21:06:18 +0800

    jumping to tips page once on PC device

--- src/main.js
+++ src/main.js
@@ -3,6 +3,22 @@ import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
 
+function IsPC() {
+  let userAgentInfo = navigator.userAgent;
+  let Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone",  "iPod");//"iPad",
+  let flag = true;
+  for (var v = 0; v < Agents.length; v++) {
+    if (userAgentInfo.indexOf(Agents[v]) > 0) {
+      flag = false;
+      break;
+    }
+  }
+  return flag;
+}
+if(IsPC()){
+  window.location = "./tips.html"
+}
+
 Vue.config.productionTip = false
 
 new Vue({
